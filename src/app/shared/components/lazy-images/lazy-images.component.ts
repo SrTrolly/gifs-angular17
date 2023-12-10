355949585eac76c87c862ae804bdd9aeac070c47
Nicolas Inostroza <ninostrozacordova2016@gmail.com>
2023-12-10 18:42:18 -0300

    seccion gifs con fernando

---
+++ src/app/shared/components/lazy-images/lazy-images.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy-images',
+  templateUrl: './lazy-images.component.html',
+  styleUrl: './lazy-images.component.css'
+})
+export class LazyImagesComponent {
+
+}
